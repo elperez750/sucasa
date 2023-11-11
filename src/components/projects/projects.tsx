ba1924c3d4edb3a97b2ad0522f6ba7928b992b9f
elperez750 <elperez750@gmail.com>
2023-11-11 01:55:59 -0800

    added cards to project and deleted some stuff

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,17 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./projects.css";
 import { Navigation } from "../navigation/navigate";
+import image1 from "../../images/attic.jpeg";
 
 export default function Projects() {
   return (
     <>
+    <body>
       <div className="image-container">
         <div className="parallax-projects"></div>
-        <h5 className='other-caption-projects-top '><span>See our work</span></h5>
+        <h5 className="other-caption-projects-top ">
+          <span>See our work</span>
+        </h5>
 
         <h1 className="project_header">Projects</h1>
-        <h2 className='other-caption-projects '><span>Explore a world where comfort meets impeccable taste.</span></h2>
-
+        <h2 className="other-caption-projects ">
+          <span>Explore a world where comfort meets impeccable taste.</span>
+        </h2>
       </div>
 
       <div className="quote-container-projects">
@@ -29,6 +34,59 @@ export default function Projects() {
           </div>
         </div>
       </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card" >
+              <img src={image1} className="project-card-img" alt="..." />
+              <div className="card-body">
+                <h2>
+                 <em> Marysville Attic </em>
+                </h2>
+                <p>
+                  Exterior Painting
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col-md-4">
+            <div className="card" >
+              <img src={image1} className="project-card-img" alt="..." />
+              <div className="card-body">
+                <h2>
+                 <em> Marysville Attic </em>
+                </h2>
+                <p>
+                  Exterior Painting
+                </p>
+              </div>
+            </div>
+          </div>
+
+
+          <div className="col-md-4">
+            <div className="card" >
+              <img src={image1} className="project-card-img" alt="..." />
+              <div className="card-body">
+                <h2>
+                 <em> Marysville Attic </em>
+                </h2>
+                <p>
+                  Exterior Painting
+                </p>
+              </div>
+            </div>
+          </div>
+
+          
+
+          
+        </div>
+      </div>
+      </body>
     </>
   );
 }
